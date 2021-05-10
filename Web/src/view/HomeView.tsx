e0df335ba09894f7588d5e5a22ac89fc1ba216d5
Lars Strömberg <stromberg.lars@gmail.com>
2021-05-10 22:46:47 +0200

    Added Calendar View

--- Web/src/view/HomeView.tsx
+++ Web/src/view/HomeView.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-export default function HomeView() {
-	return (
+const localizer = momentLocalizer(moment);
+
+class HomeView extends Component {
+	state = {
+		events: [
+			{
+				start: moment().toDate(),
+				end: moment()
+					.add(1, 'days')
+					.toDate(),
+				title: 'Some title'
+			}
+		]
+	};
+	
+	render() {
+		return (
 		<main>
 			<h1>
 				HomeView
 			</h1>
+			<Calendar
+				localizer={localizer}
+				defaultDate={new Date()}
+				defaultView="week"
+				events={this.state.events}
+				style={{ height: "100vh" }}
+			/>
 		</main>
-	);
+		);
+	};
 }
+
+export default HomeView;
