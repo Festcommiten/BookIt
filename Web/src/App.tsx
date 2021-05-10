7b20218e8ac7214fdf67ee246c9f5121e80b8c61
Lars Strömberg <stromberg.lars@gmail.com>
2021-05-10 10:01:16 +0200

    Added dependencies and "dummy" imported them

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import './utils/global/Global.css';
 import HomeView from './view/HomeView';
+// import {
+// 	BrowserRouter as Router,
+// 	Switch,
+// 	Route,
+// 	Link
+// } from "react-router-dom";
+// import axios from 'axios';
+// import Button from '@material-ui/core/Button';
+// import { Calendar, momentLocalizer } from 'react-big-calendar'
 
 function App() {
 	return (
