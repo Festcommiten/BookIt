291badfe9c3440d96f3d7ea84d99cb80b0cc4e98
Leo <gbgleo@gmail.com>
2021-05-24 22:55:25 +0200

    Work in progress, first insert works, second doesnt because of duplicate ids

--- App/generate_mockdata.py
+++ App/generate_mockdata.py
@@ -4,7 +4,7 @@ import datetime
 # MONGO
 client = MongoClient("mongodb://db:27017")
 db = client.test_db
-collection = db["mock"]
+collection = db["mock_data"]
 
 my_date = datetime.date.today()
 time_now = datetime.datetime.today()
@@ -48,7 +48,7 @@ def convert_int_to_string_of_minimum_length_two(int_input):
 
 def get_starting_id(all_int_id_entries):
     if all_int_id_entries:
-        last_json_entry = all_int_id_entries[-1]
+        last_json_entry = all_int_id_entries[:1]
         entry_id = last_json_entry["_id"]
         if type_is_int(entry_id):
             return entry_id
@@ -58,14 +58,16 @@ def get_starting_id(all_int_id_entries):
         return 0  # Case when no entries exist in db
 
 
-def int_ids_in_db():
+def latest_int_id_entry_in_db():
     try:
-        all_int_id_entries = collection.find({"_id": {"$type": "double"}})
+        latest_int_id_entry = collection.find().sort({"_id": -1}).limit(1)["_id"]
+        print(type(latest_int_id_entry))
+        print(latest_int_id_entry)
     except:
-        all_int_id_entries = False
+        latest_int_id_entry = False
 
-    if all_int_id_entries:
-        return all_int_id_entries
+    if latest_int_id_entry:
+        return latest_int_id_entry
     else:
         return False
 
@@ -78,7 +80,6 @@ def room_name_exist(room_name):
 
 
 def day_of_time_slot(slot_number):
-    day = 1
     if slot_number <= 9:
         return 1
     elif 10 <= slot_number <= 18:
@@ -101,23 +102,23 @@ def date_of_day(week: int, day: int):
 def get_starting_hour(time_slot_number):
     slot_hour = time_slot_number % 9
     if slot_hour == 1:
-        return "08"
+        return 8
     elif slot_hour == 2:
-        return "09"
+        return 9
     elif slot_hour == 3:
-        return "10"
+        return 10
     elif slot_hour == 4:
-        return "11"
+        return 11
     elif slot_hour == 5:
-        return "12"
+        return 12
     elif slot_hour == 6:
-        return "13"
+        return 13
     elif slot_hour == 7:
-        return "14"
+        return 14
     elif slot_hour == 8:
-        return "15"
+        return 15
     else:
-        return "16"
+        return 16
 
 
 def get_current_time_slot_value(week: int, time_slot_number: int):
@@ -126,19 +127,22 @@ def get_current_time_slot_value(week: int, time_slot_number: int):
     year = convert_int_to_string_of_minimum_length_two(date[0])
     month = convert_int_to_string_of_minimum_length_two(date[1])
     day = convert_int_to_string_of_minimum_length_two(date[2])
-    hour = get_starting_hour(time_slot_number)
+    hour_int = get_starting_hour(time_slot_number)
+    hour = convert_int_to_string_of_minimum_length_two(hour_int)
     minute = "00"
 
     starting_time = f"{year}-{month}-{day}T{hour}:{minute}:00+02:00"
-    end_time = f"{year}-{month}-{day}T{hour + 1}:{minute}:00+02:00"
+    end_time = f"{year}-{month}-{day}T{convert_int_to_string_of_minimum_length_two(hour_int+1)}:{minute}:00+02:00"
     returnArray = [starting_time, end_time]
     return returnArray
 
 
-def generate_empty_documents_for_room_time_slots_based_week(room_name: str, week: int):
+
+def generate_empty_documents_for_room_time_slots_based_on_week(room_name: str, week: int):
     room_data_for_week = []
-    all_int_id_entries = int_ids_in_db()
-    counter_for_id =  get_starting_id(all_int_id_entries)
+    counter_for_id = latest_int_id_entry_in_db() + 1
+    if not type_is_int(counter_for_id):
+        counter_for_id = 1
     if room_name_exist(room_name):
         for i in range(TIME_SLOTS_FOR_ROOM_PER_WEEK):
             room_empty_time_slot_data = {
@@ -155,14 +159,44 @@ def generate_empty_documents_for_room_time_slots_based_week(room_name: str, week
     return room_data_for_week
 
 
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[0]))
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[1]))
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[2]))
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[3]))
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[4]))
-collection.insertMany(generate_empty_documents_for_room_time_slots_based_week(ROOM_NAMES_LIST[0], weeks[5]))
-
-
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[0]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[1]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[2]))
+"""
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[3]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[4]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[0], weeks[5]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[0]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[1]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[2]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[3]))
+collection.insert(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[4]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[1], weeks[5]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[0]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[1]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[2]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[3]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[4]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[2], weeks[5]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[0]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[1]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[2]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[3]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[4]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[3], weeks[5]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[0]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[1]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[2]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[3]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[4]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[4], weeks[5]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[0]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[1]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[2]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[3]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[4]))
+collection.insert_many(generate_empty_documents_for_room_time_slots_based_on_week(ROOM_NAMES_LIST[5], weeks[5]))
+"""
 
 """
 days = datetime.timedelta(days=1)
