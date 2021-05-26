324705f3e1e956cb1137b875828ea0e4ab748a49
Lars Strömberg <stromberg.lars@gmail.com>
2021-05-26 20:34:20 +0200

    Added CORS fix to backend
    https://flask-cors.readthedocs.io/en/latest/

--- App/app_data.py
+++ App/app_data.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from helpers.populate_mock_db import insert_empty_time_slots, insert_random_bookings
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 # MONGO
 client = MongoClient("mongodb://db:27017")
@@ -21,13 +23,14 @@ except Exception:
     print("Mock data already exists.")
 
 
-
 @app.route('/')
+@cross_origin()
 def alive():
     return "Hello World!"
 
 
 @app.route('/bookings/<int:week>/<string:room_name>', methods=['GET'])
+@cross_origin()
 def get_bookings_for_room_and_week(week, room_name):
     retArray = []
     cursor = collection.find({"$and": [{"week": week}, {"room": room_name}]})
@@ -37,12 +40,14 @@ def get_bookings_for_room_and_week(week, room_name):
 
 
 @app.route('/delete/<int:id>', methods=['PUT'])
+@cross_origin()
 def delete_booking(id):
     collection.update({"_id": id}, {"$set": {"booking_company": "", "booker": ""}})
     return "Time slot now empty"
 
 
 @app.route('/new_booking/<int:id>', methods=['PUT'])
+@cross_origin()
 def new_booking(id):
     booking_info = request.json
     company = booking_info["booking_company"]
