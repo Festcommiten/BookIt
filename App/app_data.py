53a43163f192d6b6df3441a1a0c4d3b45e72a6e5
Sinjebos <Nicke_95_@hotmail.com>
2021-05-31 14:00:08 +0200

    updated dockers

--- App/app_data.py
+++ App/app_data.py
@@ -81,5 +81,5 @@ def get_users():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
-    app.run(debug=True, host='51.116.145.32', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
